8d5001f607dddb4c8f707247b9e060815b999230
tekin114 <66351183+tekin114@users.noreply.github.com>
2020-06-03 08:26:11 +0200

    sitewebGonen

---
+++ result.php
@@ -0,0 +1,14 @@
+<!DOCTYPEhtml>
+<html lang='fr'>
+	<head>
+		<meta charset="utf-8"/>
+		<title> Result</title>
+		<link href="resultStyle.css" rel="stylesheet">
+		
+	</head>
+	<body class ="demande">
+		<p>Bonjour <?php echo $_POST['nom']; ?> ! Merci pour votre demande!</p>
+
+		<p>Si vous voulez modifier votre demande, <a href="location.html">cliquez ici</a> pour revenir à la page formulaire.</p>
+	</body>		
+</html>
